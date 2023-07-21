0bfdaea738955000289612c07a64a490e15f6288
gitjav13 <jguillengbs@gmail.com>
2023-07-21 22:33:28 +0200

    hasta libreria kubernetes

--- Terraform/recursoK8s.tf
+++ Terraform/recursoK8s.tf
@@ -1,6 +1,6 @@
 # Cluster gestionado de kubernetes
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks"
+  name                = "clusterAKS"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   dns_prefix          = "Cluster"
@@ -14,4 +14,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
     type = "SystemAssigned"
   }
+}
+
+resource "azurerm_role_assignment" "enlace_acr_aks" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
 }
