bad37cfdd36f4d842896c968384709a8af6f6632
Torre1 <jguillengbs@gmail.com>
2023-07-24 17:41:55 +0200

    ignore

--- Ansible/deploy.sh
+++ Ansible/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-- ansible-playbook -i hosts 00_playbook.yaml
-- ansible-playbook -i hosts 01_playbook.yaml
-- ansible-playbook -i hosts 02_playbook.yaml
+ansible-playbook -i hosts 00_playbook.yaml
+ansible-playbook -i hosts 01_playbook.yaml
+ansible-playbook -i hosts 02_playbook.yaml
